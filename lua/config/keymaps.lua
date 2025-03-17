3c050bf70c25ed7e6c5dbb7b917e53270d2cfd89
Sriram Ramesh <sriram@vananam.com>
2025-03-17 13:05:48 +0530

    updated the keymaps to copy the abrevitated commit hash

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<C-c>", function()
+  local commit_hash = vim.fn.system("git log -1 --format=%h --abbrev=7")
+  commit_hash = commit_hash:gsub("%s+", "") -- Remove any trailing whitespace
+  vim.fn.setreg("+", commit_hash) -- Copy to the system clipboard
+  vim.notify("Copied commit hash: " .. commit_hash, vim.log.levels.INFO)
+end, { desc = "Copy abbreviated commit hash" })
