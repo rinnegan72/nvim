d8c984c1c537ce3c473798a8d613424b24993331
Sriram Ramesh <sriram@vananam.com>
2025-06-09 13:05:51 +0530

    git blame shows abbreviated commit hash as well

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -65,7 +65,7 @@ require("gitsigns").setup({
     ignore_whitespace = false,
     virt_text_priority = 100,
   },
-  current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+  current_line_blame_formatter = "<abbrev_sha> <author>, <author_time:%Y-%m-%d> - <summary>",
   sign_priority = 6,
   update_debounce = 100,
   status_formatter = nil, -- Use default
