a53b473a69c259297f55421861eb57b0507417b6
Sriram Ramesh <rinnegan.sr@gmail.com>
2025-06-22 22:58:25 +0530

    Revert "minor fixes to the dap config to address lua lsp warnings"
    This reverts commit a713cbcca40b98fecc2f78c282fdbba25c0c6cff.

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -2,47 +2,6 @@
 -- Debug Adapter Protocol (DAP) configuration
 
 return {
-  -- Core DAP
-  {
-    "mfussenegger/nvim-dap",
-    config = function()
-      local dap = require("dap")
-
-      -- Add missing required fields for daput.Config
-      dap.defaults.fallback.switchbuf = "useopen"
-      dap.defaults.fallback.external_terminal = {
-        command = "/usr/bin/alacritty",
-        args = { "-e" },
-      }
-
-      -- Add basic configuration
-      dap.configurations.lua = {
-        {
-          type = "nlua",
-          request = "attach",
-          name = "Attach to running Neovim instance",
-          host = function()
-            local value = vim.fn.input("Host [127.0.0.1]: ")
-            if value ~= "" then
-              return value
-            end
-            return "127.0.0.1"
-          end,
-          port = function()
-            local val = tonumber(vim.fn.input("Port: "))
-            assert(val, "Please provide a port number")
-            return val
-          end,
-        },
-      }
-
-      -- Set up adapters with required fields
-      dap.adapters.nlua = function(callback, config)
-        callback({ type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 })
-      end
-    end,
-  },
-
   -- DAP UI
   {
     "rcarriga/nvim-dap-ui",
@@ -53,52 +12,11 @@ return {
     config = function()
       local dapui = require("dapui")
       dapui.setup({
-        -- Add missing required fields
-        icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
-        mappings = {
-          -- Use a table to describe mappings
-          expand = { "<CR>", "<2-LeftMouse>" },
-          open = "o",
-          remove = "d",
-          edit = "e",
-          repl = "r",
-          toggle = "t",
-        },
-        element_mappings = {
-          -- element specific mappings
-          -- e.g. toggle the breakpoint for the current line in breakpoints window
-        },
-        expand_lines = vim.fn.has("nvim-0.7") == 1,
-        force_buffers = true,
-        floating = {
-          max_height = nil, -- These can be integers or a float between 0 and 1.
-          max_width = nil, -- Floats will be treated as percentage of your screen.
-          border = "single", -- Border style. Can be "single", "double" or "rounded"
-          mappings = {
-            close = { "q", "<Esc>" },
-          },
-        },
-        render = {
-          max_type_length = nil, -- Can be integer or nil.
-          max_value_lines = 100, -- Can be integer or nil.
-          indent = 1,
-        },
         -- Keep UI open after debug session ends
         close_on_end = false,
         controls = {
           enabled = true,
           element = "repl",
-          icons = {
-            pause = "⏸",
-            play = "▶",
-            step_into = "⏎",
-            step_over = "⏭",
-            step_out = "⏮",
-            step_back = "b",
-            run_last = "▶▶",
-            terminate = "⏹",
-            disconnect = "⏏",
-          },
         },
         layouts = {
           {
@@ -138,40 +56,7 @@ return {
   {
     "theHamsta/nvim-dap-virtual-text",
     config = function()
-      require("nvim-dap-virtual-text").setup({
-        enabled = true, -- enable this plugin (the default)
-        enable_commands = true,
-        enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
-        highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
-        highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
-        show_stop_reason = true, -- show stop reason when stopped for exceptions
-        commented = false, -- prefix virtual text with comment string
-        only_first_definition = true, -- only show virtual text at first definition (if there are multiple)
-        all_references = false, -- show virtual text on all all references of the variable (not only definitions)
-        clear_on_continue = false, -- clear virtual text on "continue" (might cause flickering when stepping)
-
-        -- Missing required fields based on the error message
-        text_prefix = " ", -- prefix to add to the virtual text
-        separator = " ", -- separator between the virtual text elements
-        error_prefix = " ", -- prefix to add to the virtual text on error
-        info_prefix = " ", -- prefix to add to the virtual text on info
-        virt_lines_above = false, -- show virtual lines above the real line
-        filter_references_pattern = "<module", -- don't show virtual text for references that match this lua pattern
-
-        display_callback = function(variable, buf, stackframe, node, options)
-          if options.virt_text_pos == "inline" then
-            return " = " .. variable.value
-          else
-            return variable.name .. " = " .. variable.value
-          end
-        end,
-        -- position of virtual text, see `:h nvim_buf_set_extmark()`, default tries to inline the virtual text. Use 'eol' to set to end of line
-        virt_text_pos = vim.fn.has("nvim-0.10") == 1 and "inline" or "eol",
-        all_frames = false, -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
-        virt_lines = false, -- show virtual lines instead of virtual text (will flicker!)
-        virt_text_win_col = nil, -- position the virtual text at a fixed window column (starting from the first text column) ,
-        -- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
-      })
+      require("nvim-dap-virtual-text").setup()
     end,
   },
 
@@ -180,7 +65,6 @@ return {
     "nvim-telescope/telescope-dap.nvim",
     dependencies = {
       "nvim-telescope/telescope.nvim",
-      "mfussenegger/nvim-dap",
     },
     config = function()
       -- Safely load the extension
@@ -202,20 +86,6 @@ return {
       -- Attempt to find python path
       local python_path = vim.fn.exepath("python3") or vim.fn.exepath("python") or "/usr/bin/python3"
       require("dap-python").setup(python_path)
-
-      -- Add Python configuration with required fields
-      local dap = require("dap")
-      table.insert(dap.configurations.python, {
-        type = "python",
-        request = "launch",
-        name = "Launch file",
-        program = "${file}",
-        pythonPath = function()
-          return python_path
-        end,
-        console = "integratedTerminal",
-        cwd = "${workspaceFolder}",
-      })
     end,
   },
 }
