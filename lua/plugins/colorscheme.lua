cc4fd5bb76af1d83da8fa49c2fd98f6165a1c767
Sriram Ramesh <sriram@vananam.com>
2026-01-05 11:07:29 +0530

    added catpuccin theming to neovim

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,33 @@
 return {
   {
-    "folke/tokyonight.nvim",
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
     opts = {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
+      flavour = "frappe",
+      transparent_background = true,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        telescope = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+          enabled = true,
+          indentscope_color = "",
+        },
       },
     },
+    config = function(_, opts)
+      local ok, catppuccin = pcall(require, "catppuccin")
+      if not ok then
+        vim.notify("Catppuccin failed to load!", vim.log.levels.ERROR)
+        return
+      end
+      catppuccin.setup(opts)
+      vim.cmd.colorscheme("catppuccin")
+    end,
   },
 }
